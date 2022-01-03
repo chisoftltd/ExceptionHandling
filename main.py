526dc5b826ed212c4c28cb8325314844584601a8
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-03 15:20:05 +0000

    Exception Handling - try ... except ... else ... finally

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 try:
     file = open("test_file.txt")
     test_dictionary = {"key": "value"}
-    print(test_dictionary["new_key"])
+    print(test_dictionary["key"])
 except FileNotFoundError:
     file = open("test_file.txt", "w")
     file.write("Something")
 except KeyError as error_msg:
     print(f"The key {error_msg} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File is closed.")
