bdf610034c0ee63833fd5021298102b1007e1a01
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-03 15:16:36 +0000

    except implementation - FileNotFoundError & KeyError

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 try:
     file = open("test_file.txt")
-except:
+    test_dictionary = {"key": "value"}
+    print(test_dictionary["new_key"])
+except FileNotFoundError:
     file = open("test_file.txt", "w")
     file.write("Something")
+except KeyError as error_msg:
+    print(f"The key {error_msg} does not exist")
